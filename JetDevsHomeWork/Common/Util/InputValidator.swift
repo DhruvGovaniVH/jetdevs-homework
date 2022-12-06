1d939d50d6b96ffbcf36f4f75796da259714d77c
Dhruv Govani <dhruv.govani@quokkalabs.com>
2022-12-06 19:35:44 +0530

    Validator file added

---
+++ JetDevsHomeWork/Common/Util/InputValidator.swift
@@ -0,0 +1,24 @@
+//
+//  InputValidator.swift
+//  JetDevsHomeWork
+//
+//  Created by Admin on 06/12/22.
+//
+
+import Foundation
+
+class Validator {
+    
+    enum ValidationRule: String {
+        case email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        case password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{16,}$"
+    }
+    
+    func validate(_ text: String, rule: ValidationRule) -> Bool {
+        
+        let textPred = NSPredicate(format: "SELF MATCHES %@", rule.rawValue)
+        return textPred.evaluate(with: text)
+        
+    }
+    
+}
