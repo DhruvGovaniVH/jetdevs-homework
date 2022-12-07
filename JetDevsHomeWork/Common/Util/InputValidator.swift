2ac523437a5864a0c357eb43212d85a3b074a869
Dhruv Govani <dhruv.govani@quokkalabs.com>
2022-12-07 15:37:46 +0530

    input limit updated to six due to current given password

--- JetDevsHomeWork/Common/Util/InputValidator.swift
+++ JetDevsHomeWork/Common/Util/InputValidator.swift
@@ -11,7 +11,7 @@ class Validator {
     
     enum ValidationRule: String {
         case email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        case password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{16,}$"
+        case password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$"
     }
     
     func validate(_ text: String, rule: ValidationRule) -> Bool {
