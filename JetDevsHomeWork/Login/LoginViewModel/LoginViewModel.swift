b0cbd818523adb6669fb8f9f9b05f6f7444bbbce
Dhruv Govani <dhruv.govani@quokkalabs.com>
2022-12-08 18:24:27 +0530

    API call reconfiguration using RxSwift

--- JetDevsHomeWork/Login/LoginViewModel/LoginViewModel.swift
+++ JetDevsHomeWork/Login/LoginViewModel/LoginViewModel.swift
@@ -6,19 +6,17 @@
 //
 
 import Foundation
+import RxSwift
 
 class LoginViewModel {
 
-    private var onLoginSuccess: (() -> Void)?
-    private var onShowError: ((String) -> Void)?
     private var showLoading: (() -> Void)?
     private var hideLoading: (() -> Void)?
     var loggedInUser: User?
     private lazy var repo = { LoginRepo() }()
     
-    init(onLogin: (() -> Void)?, onShowError: ((String) -> Void)?, showLoader : (() -> Void)?, hideLoader : (() -> Void)?) {
-        self.onLoginSuccess = onLogin
-        self.onShowError = onShowError
+    init(showLoader: (() -> Void)?, hideLoader: (() -> Void)?) {
+        
         self.showLoading = showLoader
         self.hideLoading = hideLoader
     }
@@ -27,32 +25,35 @@ class LoginViewModel {
     /// - Parameters:
     ///   - email: email of user
     ///   - password: password of user
-    func loginUser(_ email: String, _ password: String) {
-        _ = self.showLoading?()
-        repo.loginUser(email: email, password: password) { userData in
-            DispatchQueue.main.async { [weak self] in
-                guard let `self` = self else {
-                    return
+    func loginUser(_ email: String, _ password: String) -> Observable<User> {
+        
+        return Observable.create { observer in
+            _ = self.showLoading?()
+            self.repo.loginUser(email: email, password: password) { userData in
+                DispatchQueue.main.async { [weak self] in
+                    guard let `self` = self else {
+                        return
+                    }
+                    _ = self.hideLoading?()
+                    if let userData = userData {
+                        self.loggedInUser = userData
+                        observer.onNext(userData)
+                    } else {
+                        observer.onError(APIError.somethingWentWrong)
+                    }
                 }
-                _ = self.hideLoading?()
-                if let userData = userData {
-                    
-                    self.loggedInUser = userData
-                    _ = self.onLoginSuccess?()
-                    
-                } else {
-                    
-                    self.onShowError?("Something went wrong! Please try again")
-                    
+                
+            } onFailure: { errorMessage in
+                DispatchQueue.main.async { [weak self] in
+                    guard let `self` = self else {
+                        return
+                    }
+                    _ = self.hideLoading?()
+                    observer.onError(APIError.error(errorMessage ?? ErrorMessages.somethingWentWrong))
                 }
             }
-        } onFailure: { errorMessage in
-            DispatchQueue.main.async { [weak self] in
-                guard let `self` = self else {
-                    return
-                }
-                _ = self.hideLoading?()
-                self.onShowError?(errorMessage ?? "Something went wrong! Please try again")
+            
+            return Disposables.create {
             }
         }
     }
